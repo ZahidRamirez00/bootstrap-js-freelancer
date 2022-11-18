70613b062686be9ea17bc7d91b72c1400d118531
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-18 13:10:03 +0100

    Aquisizione del value di <select> del DOM

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Creazione degli array con i dati da utilizzare
-let inputTypework = [20.50, 15.30, 33.60];
+let costoTypework = [20.50, 15.30, 33.60];
 //Ricorda che lo sconto da fare in caso il codice inserito sia corretto è del -- 25% -- del prezzo finale
 let inputDiscountcode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
@@ -21,7 +21,16 @@ let inputDiscountcode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
 
+//=========== FUNZIONI =====================================================
 
+function calcoloCostoFinale(event){
+//Evito che submit il suo evento di Default. Perché voglio io definire il suo comportamento
+    event.preventDefault();
+//-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.
+//Aquisizione del valore dell'elemento select del DOM    
+    let inputTypework = document.getElementById("inputTypework2").value;
+    console.log("Il valore inserito nel select è: [" + inputTypework + "] che equivale a: [" + costoTypework[inputTypework-1] + "]");
 
+}
 
 
