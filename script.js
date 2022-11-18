53536ad27216bd7e641e78a73162671302831843
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-18 17:59:59 +0100

    Controllo prezzo all'ora anche con sconto

--- script.js
+++ script.js
@@ -29,18 +29,39 @@ function calcoloCostoFinale(event){
 //-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.-.-.-.-.-.-.-.-.-.-.-.
 //Aquisizione del valore dell'elemento select del DOM    
     let inputTypework = document.getElementById("inputTypework2").value;
-    console.log("Il valore inserito nel select è: [" + inputTypework + "] che equivale a: [" + costoTypework[inputTypework-1] + "]");
-
-    let inputSconto = document.getElementById("inputDiscountcode2").value.toUpperCase();
-    let controlloSconto = false;
-    console.log("Il codive inserito è: [" + inputSconto + "]");
-//Controllo del Codice Sconto
-    for(let i = 0; i < codiciSconto.length; i++){
-        if(inputSconto == codiciSconto[i]){
-            controlloSconto = true;
-        }
-    }
     
+    let costoLavoro = 0;
+
+    let inputOre = parseInt(document.getElementById("inputHours2").value);
+//Controllo
+    if(Number.isInteger(inputOre)){
+        console.log("Il valore inserito nel select è: [" + inputTypework + "] che equivale a: [" + costoTypework[inputTypework-1] + "€]");
+        let inputSconto = document.getElementById("inputDiscountcode2").value.toUpperCase();
+        let controlloSconto = false;
+        console.log("Il codive inserito è: [" + inputSconto + "]");
+    //Controllo del Codice Sconto
+        for(let i = 0; i < codiciSconto.length; i++){
+            if(inputSconto == codiciSconto[i]){
+                controlloSconto = true;
+            }
+        }
+        
+        costoLavoro = costoTypework[inputTypework-1] * inputOre;
+        console.log("Il costo all'ora del lavoro richiesto è: [" + costoLavoro + "€]");
+
+        if(controlloSconto){
+            costoLavoro = costoLavoro*0.75;
+            console.log("Il prezzo finale scontato è: [" + costoLavoro.toFixed(2) + "€]");
+        }else if(inputSconto == ""){
+            //costoLavoro = costoTypework * inputOre;
+            console.log("Il prezzo finale senza sconto è: [" + costoLavoro + "€]");
+        }else if(!controlloSconto){
+            console.log("Il prezzo finale senza sconto è: [" + costoLavoro + "€]")
+        }
+        
+    }else{
+        console.log("ERROR: Hai inserito una parola nelle ore richieste!!!!!!!!")
+    }
 }
 
 
