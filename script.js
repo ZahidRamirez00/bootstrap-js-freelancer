38b350fe4b18228016fabbedd597b7e235f2fd29
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-18 12:34:00 +0100

    Commenti all'interno del file js

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
+//Creazione degli array con i dati da utilizzare
 let inputTypework = [20.50, 15.30, 33.60];
+//Ricorda che lo sconto da fare in caso il codice inserito sia corretto è del -- 25% -- del prezzo finale
 let inputDiscountcode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
