40ebc42cdfa76bb9e5adcbff527f5429636b5445
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-18 15:55:25 +0100

    Controllo del Codice Sconto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Creazione degli array con i dati da utilizzare
 let costoTypework = [20.50, 15.30, 33.60];
 //Ricorda che lo sconto da fare in caso il codice inserito sia corretto è del -- 25% -- del prezzo finale
-let inputDiscountcode = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
+let codiciSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
 
 
@@ -31,6 +31,16 @@ function calcoloCostoFinale(event){
     let inputTypework = document.getElementById("inputTypework2").value;
     console.log("Il valore inserito nel select è: [" + inputTypework + "] che equivale a: [" + costoTypework[inputTypework-1] + "]");
 
+    let inputSconto = document.getElementById("inputDiscountcode2").value.toUpperCase();
+    let controlloSconto = false;
+    console.log("Il codive inserito è: [" + inputSconto + "]");
+//Controllo del Codice Sconto
+    for(let i = 0; i < codiciSconto.length; i++){
+        if(inputSconto == codiciSconto[i]){
+            controlloSconto = true;
+        }
+    }
+    
 }
 
 
